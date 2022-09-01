ca4e3d1a1c5cccee3e533636d90a7ea15c433435
Guillaume Bourassa <gbourassa@mirego.com>
2022-09-01 09:51:18 -0400

    Methods are now more generic

--- utils/src/main/kotlin/com/mirego/compose/utils/Thresholds.kt
+++ utils/src/main/kotlin/com/mirego/compose/utils/Thresholds.kt
@@ -2,13 +2,12 @@ package com.mirego.compose.utils
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.Dp
 
 /**
  * Lets you get values that uses breakpoints on screen width or height
  *
- * val margin = sizeWithWidthThresholds(
+ * val margin = valueWithWidthThresholds(
  *    1000.dp to 128.dp,
  *    600.dp to 64.dp,
  *    0.dp to 12.dp
@@ -16,22 +15,12 @@ import androidx.compose.ui.unit.Dp
  */
 
 @Composable
-fun sizeWithHeightThresholds(vararg thresholds: Pair<Dp, Dp>): Dp = valueWithThresholds(LocalConfiguration.current.screenHeightDp, *thresholds)
+fun <T> valueWithHeightThresholds(vararg thresholds: Pair<Dp, T>): T =
+    valueWithThresholds(LocalConfiguration.current.screenHeightDp, *thresholds)
 
 @Composable
-fun sizeWithWidthThresholds(vararg thresholds: Pair<Dp, Dp>): Dp = valueWithThresholds(LocalConfiguration.current.screenWidthDp, *thresholds)
-
-@Composable
-fun textStyleWithHeightThresholds(vararg thresholds: Pair<Dp, TextStyle>) = valueWithThresholds(LocalConfiguration.current.screenHeightDp, *thresholds)
-
-@Composable
-fun textStyleWithWidthThresholds(vararg thresholds: Pair<Dp, TextStyle>) = valueWithThresholds(LocalConfiguration.current.screenWidthDp, *thresholds)
-
-@Composable
-fun intWithHeightThresholds(vararg thresholds: Pair<Dp, Int>): Int = valueWithThresholds(LocalConfiguration.current.screenHeightDp, *thresholds)
-
-@Composable
-fun intWithWidthThresholds(vararg thresholds: Pair<Dp, Int>): Int = valueWithThresholds(LocalConfiguration.current.screenWidthDp, *thresholds)
+fun <T> valueWithWidthThresholds(vararg thresholds: Pair<Dp, T>): T =
+    valueWithThresholds(LocalConfiguration.current.screenWidthDp, *thresholds)
 
 private fun <T> valueWithThresholds(
     comparatorInDp: Int,
