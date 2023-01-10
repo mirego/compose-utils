f16303a812781c57919614bfe319b421e52dc424
Mathieu Larue <mlarue@mirego.com>
2023-01-10 11:13:18 -0500

    Preview modifier + versions update

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,5 +1,7 @@
 object Versions {
-    const val JETPACK_COMPOSE = "1.2.0-rc03"
-    const val ANDROID_GRADLE_PLUGIN = "7.3.0-rc01"
+    const val ANDROID_GRADLE_PLUGIN = "7.3.1"
     const val KTLINT = "11.0.0"
+    const val KOTLIN = "1.7.20"
+    const val COMPOSE_BOM = "2022.12.00"
+    const val KOTLIN_COMPILER = "1.3.2"
 }
