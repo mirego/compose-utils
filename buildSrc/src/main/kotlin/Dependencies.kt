92e10c085f734e5b83570348869dbc603dee6480
Mathieu Larue <mlarue@mirego.com>
2023-11-03 08:11:42 -0400

    Update dependencies + PathUtils.pathFromPathData() (#12)

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,7 +1,7 @@
 object Versions {
-    const val ANDROID_GRADLE_PLUGIN = "7.3.1"
+    const val ANDROID_GRADLE_PLUGIN = "8.1.0"
     const val KTLINT = "11.0.0"
-    const val KOTLIN = "1.7.20"
-    const val COMPOSE_BOM = "2022.12.00"
-    const val KOTLIN_COMPILER = "1.3.2"
+    const val KOTLIN = "1.9.10"
+    const val COMPOSE_BOM = "2023.10.01"
+    const val KOTLIN_COMPILER = "1.5.3"
 }
--- utils/build.gradle.kts
+++ utils/build.gradle.kts
@@ -15,10 +15,11 @@ repositories {
 group = "com.mirego.compose"
 
 android {
+    namespace = "com.mirego.compose.utils"
+
     defaultConfig {
-        compileSdk = 33
+        compileSdk = 34
         minSdk = 21
-        targetSdk = 33
     }
 
     buildFeatures {
@@ -26,14 +27,20 @@ android {
         buildConfig = false
     }
     compileOptions {
-        sourceCompatibility(JavaVersion.VERSION_11)
-        targetCompatibility(JavaVersion.VERSION_11)
+        sourceCompatibility(JavaVersion.VERSION_17)
+        targetCompatibility(JavaVersion.VERSION_17)
     }
     composeOptions {
         kotlinCompilerExtensionVersion = Versions.KOTLIN_COMPILER
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
+    }
+    publishing {
+        singleVariant("release") {
+            withJavadocJar()
+            withSourcesJar()
+        }
     }
 }
 
@@ -46,13 +53,7 @@ dependencies {
     api("androidx.compose.ui:ui")
     api("androidx.compose.animation:animation")
     api("androidx.compose.material:material")
-}
-
-tasks {
-    val sourcesJar by creating(Jar::class) {
-        archiveClassifier.set("sources")
-        from(kotlin.sourceSets["main"].kotlin.srcDirs)
-    }
+    api("androidx.core:core")
 }
 
 afterEvaluate {
@@ -61,7 +62,6 @@ afterEvaluate {
             create<MavenPublication>("release") {
                 from(components["release"])
                 artifactId = "utils"
-                artifact(tasks["sourcesJar"])
             }
         }
     }
